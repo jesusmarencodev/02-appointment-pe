e5cb60009de819fe108b5d7e4abfb2c7a881788a
Jesus Marenco <jesus.marenco@externos-cl.cencosud.com>
2022-08-11 16:47:07 -0500

    eliminando conexion directa del eventBridge y agregando conexion con la cola de peru

--- src/functions/appointment/index.ts
+++ src/functions/appointment/index.ts
@@ -2,17 +2,20 @@ import { getPathHandler } from "../libs/getPathHandler";
 
 export default {
   handler: `${getPathHandler(__dirname)}/handler.appointmentHandler`,
-  events :[
+  events: [
     {
-      eventBridge : {
-        //colocamos el arn del eventBridge que ya tenemsos creado su nombre es EventBusPracticaAWSServerless01
-        eventBus: "arn:aws:events:us-east-1:867855303039:event-bus/EventBusPracticaAWSServerless01",
-        pattern:{
-          source:["appointment"],
-          "detail-type": ["appointment-create-pe"]
-        },
-        deadLetterQueueArn: "${ssm:/digital/sql-dlq-deployment-name-${self:provider.stage}}",
+      sqs: {
+        arn: "${ssm:/digital/sqs-pe-arn-${self:provider.stage}}"
       }
+      /*       eventBridge : {
+              //colocamos el arn del eventBridge que ya tenemsos creado su nombre es EventBusPracticaAWSServerless01
+              eventBus: "arn:aws:events:us-east-1:867855303039:event-bus/EventBusPracticaAWSServerless01",
+              pattern:{
+                source:["appointment"],
+                "detail-type": ["appointment-create-pe"]
+              },
+              deadLetterQueueArn: "${ssm:/digital/sql-dlq-deployment-name-${self:provider.stage}}",
+            } */
     }
   ]
 };
